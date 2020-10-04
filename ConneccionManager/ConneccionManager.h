bca99c3136894d89230f19a18a763ba344be9e1c
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-04 13:22:16 -0500

    Initial Commit

---
+++ ConneccionManager/ConneccionManager.h
@@ -0,0 +1,18 @@
+//
+//  ConneccionManager.h
+//  ConneccionManager
+//
+//  Created by Miguel Mexicano Herrera on 04/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConneccionManager.
+FOUNDATION_EXPORT double ConneccionManagerVersionNumber;
+
+//! Project version string for ConneccionManager.
+FOUNDATION_EXPORT const unsigned char ConneccionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConneccionManager/PublicHeader.h>
+
+
